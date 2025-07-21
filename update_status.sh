e3c4a279a449acd87e358707a000391e0a01a364
akhalid-twi <akhalid@thewaterinstitute.org>
2025-07-21 18:51:20 -0400

    results notebook S0160

--- update_status.sh
+++ update_status.sh
@@ -1,14 +1,13 @@
-#!bin/bash
+#!/bin/bash
 
-
-# run to get the status update
+# Run the Python script to get the status update
 python review_model_simulations.py
 
-# upload the new csv
+# Get the current date and time
+datetime=$(date +"%m/%d @ %I:%M%p")
+
+# Upload the new CSV with a dynamic commit message
 git add erdc_baseline_simulation_summary.csv
-git commit -m "updated production log"
+git commit -m "updated production log $datetime"
 git push
-
-# provide login for github
-
 
