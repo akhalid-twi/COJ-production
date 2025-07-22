4f4926f55aa40b2772720824826df900263cd29d
arslaan-twi <akhalid@thewaterinstitute.org>
2025-07-22 12:53:03 -0400

    Update production_status-app.py

--- production_status-app.py
+++ production_status-app.py
@@ -44,7 +44,7 @@ st.dataframe(styled_df, use_container_width=True)
 
 st.subheader("Available QC files to Review")
 
-notebook_url = "https://github.com/akhalid-twi/COJ-production/blob/a6fc0713035084895f43efde2e3915ecd67960e5/example_qc/results_S0155_notebook.ipynb"
+notebook_url = "https://github.com/akhalid-twi/COJ-production/blob/a6fc0713035084895f43efde2e3915ecd67960e5/example_qc/results_S0155_notebook.html"
 st.markdown(f'<a href="{notebook_url}" target="_blank">🔗 View Notebook for S0155</a>', unsafe_allow_html=True)
 
 
