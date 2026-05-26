d6256e9c8942d19973c36f0ce71b16846989b56b
akhalid-twi <akhalid@thewaterinstitute.org>
2026-05-26 12:22:05 -0400

    updated production log 05/26 @ 12:22PM

--- production_status-app.py
+++ production_status-app.py
@@ -103,8 +103,8 @@ SCENARIOS = {
     "synthetic_nontc_slr1": {
         "title": "Synthetic Non-TC - SLR - IntLow 2040",
         "start_date": datetime(2026, 5, 23),
-        "completion_date_projected": datetime(2026, 5, 8),
-        "completion_date_actual": datetime(2026, 5, 28),
+        "completion_date_projected": datetime(2026, 5, 28),
+        "completion_date_actual": datetime(2026, 5, 27),
         "total_simulations": 3648,
     },
 
